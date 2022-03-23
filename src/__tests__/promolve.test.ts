098ad3e4b6e85d4df7dba8fb8c7acdf263ab4c4b
Joachim Flottorp <joachimflottorp@gmail.com>
2022-03-23 18:49:52 +0100

    Format + rename file

--- src/__tests__/promolve.test.ts
+++ src/__tests__/promolve.test.ts
@@ -3,36 +3,36 @@ import { Promolve, IReject, IResolve } from '../index.js';
 const DEFAULT_ERROR = 'This promise failed!';
 
 function ResolveCB(resolve: IResolve<number>) {
-	const a = 5;
-	const b = 10;
+    const a = 5;
+    const b = 10;
 
-	resolve(a + b);
+    resolve(a + b);
 }
 
 function RejectCB(reject: IReject) {
-	const error = new Error(DEFAULT_ERROR);
+    const error = new Error(DEFAULT_ERROR);
 
-	reject(error);
+    reject(error);
 }
 
 test('Resolve value is number', () => {
-	const p = Promolve<number>();
+    const p = Promolve<number>();
 
-	p.promise.then((value) => {
-		expect(value).toBe(15);
-		expect(typeof value).toBe('number');
-	});
+    p.promise.then((value) => {
+        expect(value).toBe(15);
+        expect(typeof value).toBe('number');
+    });
 
-	ResolveCB(p.resolve);
+    ResolveCB(p.resolve);
 });
 
 test('Reject value should be Error', () => {
-	const p = Promolve();
+    const p = Promolve();
 
-	p.promise.catch((error) => {
-		expect(error.message).toBe(DEFAULT_ERROR);
-		expect(error instanceof Error).toBe(true);
-	});
+    p.promise.catch((error) => {
+        expect(error.message).toBe(DEFAULT_ERROR);
+        expect(error instanceof Error).toBe(true);
+    });
 
-	RejectCB(p.reject);
+    RejectCB(p.reject);
 });
